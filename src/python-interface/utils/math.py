e0e9db2df9159c81808eb66caea08905228857fd
Qusai Hroub <qusaihroub.r@gmail.com>
2021-02-01 18:05:27 +0200

    state-stuffs/Xreate python interface for communicating with math module

---
+++ src/python-interface/utils/math.py
@@ -0,0 +1,29 @@
+""" python-interface/utils/math.py
+
+ This file is part of Reinforcement Learning-based Rotary Inverted Pendulum
+ Graduation Project.
+
+ Authors:
+ Qusai Hroub <qusaihroub.r@gmail.com>
+
+ This program is free software; you can redistribute it and/or modify it
+ under the terms and conditions of the GNU General Public License,
+ version 3, as published by the Free Software Foundation.
+
+ This program is distributed in the hope it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ more details.
+"""
+
+import ctypes
+from ctypes import cdll
+
+UTILITY_PATH = './math.so'
+utility = cdll.LoadLibrary(UTILITY_PATH)
+
+utility.angle_normalize.argtypes = [ctypes.c_double]
+utility.angle_normalize.restype = ctypes.c_double
+
+def angle_normalize (angle):
+    return utility.angle_normalize(angle)

26c30c1cd54e0453ceb367bf1a8cd7190dc7ccc9
Qusai <qusaihroub.r@gmail.com>
2021-04-24 07:50:53 +0100

    Create QThread class

---
+++ src/utils/qthread.hh
@@ -0,0 +1,52 @@
+/* util/qthread.hh
+ *
+ * This file is part of Reinforcement Learning-based Rotary Inverted Pendulum
+ * Graduation Project.
+ *
+ * Authors:
+ * Qusai Hroub <qusaihroub.r@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 3, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ */
+
+#ifndef QTHREAD_HH
+#define QTHREAD_HH
+
+#include <thread>
+#include <atomic>
+
+using namespace std;
+
+class QThread {
+	atomic<bool> active{true};
+
+    public:
+        void start(auto);
+        void stop();
+};
+
+void QThread::start(auto function) {
+    active = true;
+    thread t([=]() {
+        while(active.load()) {
+            if(!active.load()) {
+            	return;
+            }
+            function();
+        }
+    });
+    t.detach();
+}
+
+void QThread::stop() {
+    active = false;
+}
+
+#endif

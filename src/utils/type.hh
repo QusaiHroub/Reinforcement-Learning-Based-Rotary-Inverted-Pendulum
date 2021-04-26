1999cae45dd156e825e179ce35ea2aa586f742d1
Qusai Hroub <qusaihroub.r@gmail.com>
2021-04-26 10:32:28 +0300

    Create basic types for Q-Table

---
+++ src/utils/type.hh
@@ -0,0 +1,133 @@
+#ifndef TYPE
+#define TYPE
+
+typedef char int_8b;
+typedef unsigned char uint_8b;
+typedef float float_4b;
+typedef double float_8b;
+typedef long double float_12_16b;
+typedef short int_16b;
+typedef unsigned short uint_16b;
+
+template<typename AType, typename QType> class ActionNode {
+  AType mAction;
+  QType mQ_value;
+  ActionNode *mLeft = nullptr;
+  ActionNode *mRight = nullptr;
+
+  uint_16b mHeight; 
+
+  public: 
+  ~ActionNode() {
+    if (mLeft) {
+      delete mLeft;
+      mLeft = nullptr;
+    }
+    
+    if (mRight) {
+      delete mRight;
+      mRight = nullptr; 
+    }
+  }
+
+  void setAction(AType action) {
+    mAction = action;
+  }
+
+  void setQValue(QType q_value) {
+    mQ_value = q_value;
+  }
+
+  void setLeft(ActionNode *left) {
+    mLeft = left;
+  }
+
+  void setRight(ActionNode *right) {
+    mRight = right;
+  }
+
+  void setHeight(uint_16b height) {
+    mHeight = height;
+  }
+
+  AType getAction() {
+    return mAction;
+  }
+
+  QType getQValue() {
+    return mQ_value;
+  }
+
+  ActionNode* getLeft() const {
+    return mLeft;
+  }
+
+  ActionNode* getRight() const {
+    return mRight;
+  }
+
+  uint_16b getHeight() const {
+    return mHeight;
+  } 
+};
+
+template<typename TreeType, typename StateType> class StateNode {
+  StateType mState;
+  TreeType *mTree = nullptr;
+  StateNode *mLeft = nullptr;
+  StateNode *mRight = nullptr;
+
+  uint_16b mHeight;
+
+  public: 
+  ~StateNode() {
+    delete mTree;
+    delete mLeft;
+    delete mRight;
+    mTree = nullptr;
+    mLeft = nullptr;
+    mRight = nullptr;
+  }
+
+  void setState(StateType state) {
+    mState = state;
+  }
+
+  void setChaildTree(TreeType *tree) {
+    mTree = tree;
+  }
+
+  void setLeft(StateNode *left) {
+    mLeft = left;
+  }
+
+  void setRight(StateNode *right) {
+    mRight = right;
+  }
+
+  void setHeight(uint_16b height) {
+    mHeight = height;
+  }
+
+  StateType getState() {
+    return mState;
+  }
+
+  TreeType* getChaildTree() {
+    return mTree;
+  }
+
+  StateNode* getLeft() const {
+    return mLeft;
+  }
+
+  StateNode* getRight() const {
+    return mRight;
+  }
+
+  uint_16b getHeight() const {
+    return mHeight;
+  } 
+};
+
+#endif

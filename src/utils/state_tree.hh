c44abcff354d252b6985a41828f05b6b442041e9
Qusai Hroub <qusaihroub.r@gmail.com>
2021-04-26 11:00:55 +0300

    Create State Tree

---
+++ src/utils/state_tree.hh
@@ -0,0 +1,96 @@
+#ifndef STATE_TREE
+#define STATE_TREE
+
+template<typename SNType, typename TType, typename SType> class StateTree {
+  SNType *mRoot = nullptr;
+  SNType *returnNode = nullptr;
+
+  SNType* getNode(SNType *root, SType state);
+  SNType* inseart(SNType *root, SType state);
+  void clear(SNType *root);
+
+  public:
+  ~StateTree();
+
+  SNType* inseart(SType state);
+  SNType* getNode(SType state);
+  void clear();
+};
+
+#include "type.hh"
+#include "math.hh"
+#include "utils.hh"
+
+template<typename SNType, typename TType, typename SType>
+SNType* StateTree<SNType, TType, SType>::getNode(SNType *root, SType state) {
+  if (!root) {
+    return nullptr;
+  }
+
+  if (state == root->getState()) {
+    return root;
+  }
+
+  if (state < root->getState()) {
+    return getNode(root->getLeft(), state);
+  }
+  
+  return getNode(root->getRight(), state);
+}
+
+template<typename SNType, typename TType, typename SType>
+SNType* StateTree<SNType, TType, SType>::inseart(SNType *root, SType state) {
+  if (root == nullptr) {
+    root = new SNType();
+    root->setState(state);
+    root->setChaildTree(new TType());
+    root->setLeft(nullptr);
+    root->setRight(nullptr);
+    
+    returnNode = root;
+    return root;
+  }
+
+  if (state == root->getState()) {
+    returnNode = root;
+    return root;
+  }
+
+  if (state < root->getState()) {
+    root->setLeft(inseart(root->getLeft(), state));
+  } else {
+    root->setRight(inseart(root->getRight(), state));
+  }
+
+  return root; 
+}
+
+template<typename SNType, typename TType, typename SType>
+void StateTree<SNType, TType, SType>::clear(SNType *root) {
+  if (root != nullptr) {
+    delete root;
+  }
+}
+
+template<typename SNType, typename TType, typename SType>
+StateTree<SNType, TType, SType>::~StateTree() {
+  clear(mRoot);
+}
+
+template<typename SNType, typename TType, typename SType>
+SNType* StateTree<SNType, TType, SType>::inseart(SType state) {
+  mRoot = inseart(mRoot, state);
+  return returnNode;
+}
+
+template<typename SNType, typename TType, typename SType>
+SNType* StateTree<SNType, TType, SType>::getNode(SType state) {
+  return getNode(mRoot, state);
+}
+
+template<typename SNType, typename TType, typename SType>
+void StateTree<SNType, TType, SType>::clear() {
+  clear(mRoot);
+}
+
+#endif

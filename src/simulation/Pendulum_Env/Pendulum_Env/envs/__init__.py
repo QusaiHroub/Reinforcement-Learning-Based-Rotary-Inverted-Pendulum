a52ee4684e5b31e4884041183f27d2d87101db6d
Qusai Hroub <qusaihroub.r@gmail.com>
2021-05-25 20:26:26 +0300

    Add rotary pendulum env

---
+++ src/simulation/Pendulum_Env/Pendulum_Env/envs/__init__.py
@@ -0,0 +1,20 @@
+""" simulation/Pendulum_Env/Pendulum_Env/envs/__init__.py
+
+ This file is part of Reinforcement Learning-based Rotary Inverted Pendulum
+ Graduation Project.
+
+ Authors:
+ Qusai Hroub <qusaihroub.r@gmail.com>
+
+ This program is free software; you can redistribute it and/or modify it
+ under the terms and conditions of the GNU General Public License,
+ version 3, as published by the Free Software Foundation.
+
+ This program is distributed in the hope it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ more details.
+"""
+
+
+from Pendulum_Env.envs.pendulum_env import PendulumEnv
---
+++ src/simulation/Pendulum_Env/Pendulum_Env/envs/utils/__init__.py
@@ -0,0 +1,21 @@
+""" simulation/Pendulum_Env/Pendulum_Env/envs/utils/__init__.py
+
+ This file is part of Reinforcement Learning-based Rotary Inverted Pendulum
+ Graduation Project.
+
+ Authors:
+ Qusai Hroub <qusaihroub.r@gmail.com>
+
+ This program is free software; you can redistribute it and/or modify it
+ under the terms and conditions of the GNU General Public License,
+ version 3, as published by the Free Software Foundation.
+
+ This program is distributed in the hope it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ more details.
+"""
+
+
+from Pendulum_Env.envs.utils.rotary_pendulum import solveRL
+from Pendulum_Env.envs.utils.pmath import angle_normalize
